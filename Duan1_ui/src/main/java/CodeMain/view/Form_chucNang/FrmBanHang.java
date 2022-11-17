d10a7af5f92b796a033d1d533d59403adff9825c
Hungsilver <hungsilvertq@gmail.com>
2022-11-17 17:18:47 +0700

    update db and code

--- Duan1_ui/src/main/java/CodeMain/view/Form_chucNang/FrmBanHang.java
+++ Duan1_ui/src/main/java/CodeMain/view/Form_chucNang/FrmBanHang.java
@@ -405,7 +405,7 @@ public class FrmBanHang extends javax.swing.JPanel {
                 .addGap(18, 18, 18)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(20, Short.MAX_VALUE))
-            .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+            .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, 549, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
