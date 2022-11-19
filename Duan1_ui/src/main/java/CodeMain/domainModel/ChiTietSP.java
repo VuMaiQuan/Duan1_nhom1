de69517b9ea5e9f864ffa05d6c158a070eaefe1a
Hungsilver <hungsilvertq@gmail.com>
2022-11-20 00:49:26 +0700

    new db

--- Duan1_ui/src/main/java/CodeMain/domainModel/ChiTietSP.java
+++ Duan1_ui/src/main/java/CodeMain/domainModel/ChiTietSP.java
@@ -18,7 +18,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import net.bytebuddy.utility.nullability.MaybeNull;
 import org.hibernate.annotations.GenericGenerator;
 
 /**
